b77cda418ba238e11a8d7f2b5489c10560b24bfb
LimaCaioOliveira <oliveira.caiolima@gmail.com>
2022-10-03 17:07:43 -0300

    Primeiras operações em JAVA
    Até o momento declarei as variáveis e solicitei o usuário que insira o operador

--- Calculadora-simples/src/App.java
+++ Calculadora-simples/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner; //para criar o objeto do tipo Scanner
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //declaração de variáveis operandos e operadores
+        char operador; //operador matemático
+        double operando1, operando2, resultado; //operandos e resultado da operação matemática, economia de linhas
+
+        //Solicitação dos valores e operação
+        Scanner reader = new Scanner(System.in);
+        System.out.println("-----------------------------------------------");
+        System.out.println("Operadores da calculadora: ");
+        System.out.println("Adição [+]\nSubtração [-]\nMultiplicação [*]\nDivisão [/]\nExponenciação [^]\nResto da divisão [%] ");
+        System.out.println("-----------------------------------------------");
+        System.out.println("Digite somente o operador: ");
+        operador = reader.next().charAt(0); //recebe e armazena a informação do primeiro caracter digitado
+
     }
 }
